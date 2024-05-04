87c8d564b09560d3ead3137bde9609f3610e2649
MUZAMMIL RAUF <168647034+Searchlink123@users.noreply.github.com>
2024-05-04 18:08:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let navbar = document.querySelector("#navbar")
+// window.addEventListener("scroll", ()=>{
+//     if(window.scrollY > 10)
+//     {
+//         navbar.classList.add("bgnav")
+//     }
+//     else{
+//         navbar.classList.remove("bgnav")
+//     }
+
+// })
